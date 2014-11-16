a07c88c88f35fce640b8bcf1a6a4be316959e60c
Jorenilson <jorneilsonlopes@gmail.com>
2014-11-16 04:09:10 -0400

    Implementação do Sprint referente ao empréstimo de Material.

--- Codigo/Web/src/br/com/samsung/mb/UsuarioMB.java
+++ Codigo/Web/src/br/com/samsung/mb/UsuarioMB.java
@@ -1,24 +1,22 @@
 package br.com.samsung.mb;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
-import javax.persistence.EntityManager;
+import javax.inject.Inject;
+import javax.inject.Named;
 
-import br.com.samsung.dao.JPAUtil;
-import br.com.samsung.dao.UsuarioDao;
 import br.com.samsung.model.Usuario;
 
-@SessionScoped
-@ManagedBean
+@Named
 public class UsuarioMB implements Serializable {
+	@Inject
+	Usuario usuario;
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}	
 }
